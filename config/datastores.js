8eeea1fc782a6ee4ba74eca0bad29b73cd561a89
nith271670 <nimisha.thilak@elektrobit.com>
2019-01-04 15:59:02 +0530

    fixed build error

--- config/datastores.js
+++ config/datastores.js
@@ -57,7 +57,7 @@ module.exports.datastores = {
   },
   mongodb:{
     adapter: 'sails-mongo',
-    url: 'mongodb://<dbuser>:<dbpassword>@ds027348.mlab.com:27348/sailsmongo' //'mongodb://localhost:27017/sailsmongo'
+    url: 'mongodb://nith271670:December@2018>@ds027348.mlab.com:27348/sailsmongo' 
   },
 
 
